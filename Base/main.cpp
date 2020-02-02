a2c58e7f548d92b2b880abc12b6f6c18ca40770a
Oscar <drumssand@gmail.com>
2020-02-02 15:40:11 -0600

    Initial Commit

---
+++ Base/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Base
+//
+//  Created by Oscar Sandoval on 02/02/20.
+//  Copyright © 2020 Oscar Sandoval. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
